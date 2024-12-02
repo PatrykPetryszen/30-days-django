8227659b702e697de2bfbf8efc6ae70076e119d0
Patryk Petryszen <ppetryszen@macbook-pro-patryk.home>
2024-12-02 09:59:34 +0100

    Day3 - Define CustomUser model with validators for shoe_size

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CustomUser #. means "current package/directory"
 
+admin.site.register(CustomUser)
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 # You define database models here (e.g., CustomUser).
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    # Add additional fields if needed
+    phone_number = models.CharField(max_length=15, blank=True)
+    shoe_size = models.PositiveIntegerField(null =True,
+        validators=[
+            MinValueValidator(30, message='Shoe size cannot be smaller than 30'),
+            MaxValueValidator(50, message='Shoe size cannot be larger than 50')
+        ]
+    )
